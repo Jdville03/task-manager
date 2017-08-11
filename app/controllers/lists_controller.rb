252f71fd42dcf2e86c545686e6d4db81d04655d7
Jdville03 <jarrel.delottinville@gmail.com>
2017-08-10 21:40:46 -0400

    update navbar

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,4 +6,24 @@ class ListsController < ApplicationController
     @lists = current_user.lists
   end
 
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      @lists = List.all
+      render :index
+    end
+  end
+
+  private
+
+    def list_params
+      params.require(:list).permit(:name)
+    end
+
 end
