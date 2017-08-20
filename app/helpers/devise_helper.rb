0c1e7babb5931d0a3d3055c381eb80bde949295e
Jdville03 <jarrel.delottinville@gmail.com>
2017-08-20 18:56:46 -0400

    create tasks index page

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -8,7 +8,7 @@ module DeviseHelper
                       :resource => resource.class.model_name.human.downcase)
 
     html = <<-HTML
-    <div class="alert alert-danger alert-dismissable" role="alert">
+    <div class="alert alert-danger alert-dismissable fade in" role="alert">
       <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
       <p>
         <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
