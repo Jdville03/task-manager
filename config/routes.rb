1c5189752cb9a56957e8c4b9fcba91870757dc37
Jdville03 <jarrel.delottinville@gmail.com>
2017-08-23 03:55:07 -0400

    add nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   resources :tags
   resources :tasks
-  resources :lists
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root 'lists#index'
+  resources :lists do
+    resources :tasks
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
