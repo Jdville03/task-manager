0a4ef0d8e7d3b7a462296ffc6e459ed7b7e339f1
Jdville03 <jarrel.delottinville@gmail.com>
2017-08-25 05:19:07 -0400

    update tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,33 @@
 class TasksController < ApplicationController
 
+  def create
+    @list = List.find(params[:list_id])
+    @task = @list.tasks.build(task_params)
+    if @task.save
+      redirect_to list_path(@list)
+    else
+      render "lists/show"
+    end
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to list_path(@task.list)
+  end
+
   def show
     @list = List.find(params[:list_id])
     @lists = current_user.lists
     @task = Task.find(params[:id])
   end
+
+  
+
+  private
+
+    def task_params
+      params.require(:task).permit(:description, :status)
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :tags
-  resources :tasks
+  #resources :tasks
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root 'lists#index'
   resources :lists do
