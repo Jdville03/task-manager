6fe91a7dfb10f524b2912bb1143d4bc36dd6d8c6
Jdville03 <jarrel.delottinville@gmail.com>
2017-08-26 16:49:42 -0400

    add checkbox to task

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,9 @@
 module TasksHelper
+
+  def form_for_task_status(task)
+    form_for([task.list, task]) do |f|
+      f.check_box :status, :class => "toggle", :checked => task.complete?
+    end
+  end
+
 end
