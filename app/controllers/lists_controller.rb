53a5260fba32714ae09e160d56e03086d75981c5
Jdville03 <jarrel.delottinville@gmail.com>
2017-08-28 06:21:20 -0400

    update list edit modal

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -29,6 +29,12 @@ class ListsController < ApplicationController
     @task = Task.new
   end
 
+  def update
+    @list = List.find(params[:id])
+    @list.update(list_params)
+    redirect_to list_path(@list)
+  end
+
   private
 
     def list_params
