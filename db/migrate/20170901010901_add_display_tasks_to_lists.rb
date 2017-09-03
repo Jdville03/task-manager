d3cbebe2eaf2ede764a94565372b5be1d048d602
Jdville03 <jarrel.delottinville@gmail.com>
2017-09-03 03:49:35 -0400

    update schema

--- db/migrate/20170901010901_add_display_tasks_to_lists.rb
+++
@@ -1,5 +0,0 @@
-class AddDisplayTasksToLists < ActiveRecord::Migration[5.1]
-  def change
-    add_column :lists, :display_tasks, :integer, default: 1
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170901010901) do
+ActiveRecord::Schema.define(version: 20170828051447) do
 
   create_table "lists", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "display_tasks", default: 1
   end
 
   create_table "tags", force: :cascade do |t|
