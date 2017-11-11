dc269d087923344c7da0a44e0293f45f5a3dcab4
Jdville03 <jarrel.delottinville@gmail.com>
2017-11-10 21:39:24 -0500

    add user serializer / add attributes and associations to serializers

--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -1,3 +1,6 @@
 class TaskSerializer < ActiveModel::Serializer
-  attributes :id, :description, :status, :priority, :due_date, :note, :user_id, :list_id
+  attributes :id, :description, :status, :priority, :due_date, :note
+  belongs_to :list
+  belongs_to :assigned_user
+  has_many :users
 end
