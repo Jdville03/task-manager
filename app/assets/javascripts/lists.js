b22670b34060c3507f04a20dba07c98a100b8f06
Jdville03 <jarrel.delottinville@gmail.com>
2017-11-13 02:06:42 -0500

    translate list JSON response into JS model object and create prototype method

--- app/assets/javascripts/lists.js
+++ app/assets/javascripts/lists.js
@@ -1,21 +1,39 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
+function List(attributes) {
+  this.id = attributes.id;
+  this.name = attributes.name;
+}
+
+document.addEventListener("turbolinks:load", function() {
+  List.templateSource = document.getElementById("list-template").innerHTML;
+  List.template = Handlebars.compile(List.templateSource);
+  List.templateSourceNav = document.getElementById("list-template-lists-nav").innerHTML;
+  List.templateNav = Handlebars.compile(List.templateSourceNav);
+});
+
+List.prototype.renderLink = function() {
+  return List.template(this);
+}
+
+List.prototype.renderLinkNav = function() {
+  return List.templateNav(this);
+}
+
 document.addEventListener("turbolinks:load", function() {
   $('#new_list').submit(function(event) {
     event.preventDefault();
     let values = $(this).serialize();
     let posting = $.post(this.action, values);
     posting.success(function(data) {
-      let list = data;
-      let template = Handlebars.compile(document.getElementById("list-template").innerHTML);
-      let templateNav = Handlebars.compile(document.getElementById("list-template-lists-nav").innerHTML);
-      let result = template(list);
-      let resultNav = templateNav(list);
-      if (list.name) {
-        $("#new-list-json").append(result);
+      let list = new List(data);
+      let listLink = list.renderLink();
+      let listLinkNav = list.renderLinkNav();
+      if (data.name) {
+        $("#new-list-json").append(listLink);
         document.getElementById("new_list").reset();
-        $("#new-list-nav-json").append(resultNav);
+        $("#new-list-nav-json").append(listLinkNav);
       }
     });
   });
